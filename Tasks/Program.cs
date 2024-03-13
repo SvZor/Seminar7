02243aaf6a9df494e6684db3f2d03fad64aa2377
SvZor <zorinasp@yandex.ru>
2024-03-13 14:39:47 +0900

    добавлена задача со строкой

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -68,5 +68,41 @@
 
 // PrintNumbers(firstNum, num);
 
+// Считать строку с консоли, содержащую латинские буквы.
+// Вывести на экран согласные буквы этой строки.
+// Указание
+// Использовать рекурсию. Не использовать цикл.
+// Пример
+// “hello” => h l l
+// “World” => W r l d
+// “Hello world!” => H l l w r l d
+
+
+Console.WriteLine("Введите строку");
+string userInput = Console.ReadLine();
+
+string vowels= "aieuo";
+string result ="";
+int i=0;
+
+void PrintUserInput(int countElement,string str)
+{
+if(str.Length==countElement)
+{
+return;
+}
+
+if(!vowels.Contains(str[countElement]))
+{
+result = result+str[countElement];
+}
+
+PrintUserInput(countElement+1,str);
+}
+
+PrintUserInput(i,userInput);
+
+System.Console.WriteLine(result);
+
 
 
